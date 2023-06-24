4b3f5fa62beb7d7d3a470bfc790bec4bfdf768df
Anastasya Lyah <lyahanastasya@gmail.com>
2023-06-24 10:58:55 +0300

    Добавьте файлы проекта.

---
+++ LyahAnastasia2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void CompareFiles(string file1, string file2, string outputFile)
+    {
+        string[] words1 = File.ReadAllText(file1).Split(' ');
+        string[] words2 = File.ReadAllText(file2).Split(' ');
+
+        var mismatchedWords = words1.Except(words2).Concat(words2.Except(words1));
+
+        File.WriteAllLines(outputFile, mismatchedWords);
+    }
+
+    static void Main(string[] args)
+    {
+        string file1 = "C:\\Users\\Анастасія\\OneDrive\\Рабочий стол\\F1.txt"; 
+        string file2 = "C:\\Users\\Анастасія\\OneDrive\\Рабочий стол\\F2.txt"; 
+        string outputFile = "C:\\Users\\Анастасія\\OneDrive\\Рабочий стол\\G.txt"; 
+
+        CompareFiles(file1, file2, outputFile);
+
+        Console.WriteLine("Збiги та вiдмiнностi записано у файлi G.txt");
+    }
+}
